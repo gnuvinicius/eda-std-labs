9d331f199c18f9a8eb4384a2575669c381a43209
Vinicius Claudino <gnu.vinicius@gmail.com>
2026-03-05 20:05:30 -0300

    refatorado codigo do mscatalog

--- mscatalog/src/main/java/br/dev/garage474/mscatalog/controller/BrandController.java
+++ mscatalog/src/main/java/br/dev/garage474/mscatalog/controller/BrandController.java
@@ -1,6 +1,5 @@
 package br.dev.garage474.mscatalog.controller;
 
-import java.net.URI;
 import java.util.UUID;
 
 import org.springdoc.core.annotations.ParameterObject;
@@ -8,19 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.dev.garage474.mscatalog.dto.BrandCreateDto;
 import br.dev.garage474.mscatalog.dto.BrandDto;
 import br.dev.garage474.mscatalog.services.BrandService;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/brands")
@@ -44,6 +37,5 @@ public class BrandController {
         BrandDto dto = brandService.getById(id);
         return ResponseEntity.ok(dto);
     }
-
 
 }
