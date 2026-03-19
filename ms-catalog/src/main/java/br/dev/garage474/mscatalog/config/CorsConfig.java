bbb7f563ccf6b76d14c4455a0aacc8578967ac2b
Vinicius Claudino <gnu.vinicius@gmail.com>
2026-03-19 09:40:42 -0300

    cors - ms-catalog

--- ms-catalog/src/main/java/br/dev/garage474/mscatalog/config/CorsConfig.java
+++ ms-catalog/src/main/java/br/dev/garage474/mscatalog/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:*")
-                .allowedMethods("GET, POST, PUT, DELETE, OPTIONS")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
