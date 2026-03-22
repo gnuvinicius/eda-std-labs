7093a3a55911b2a113578a59c4d2ee1f78246890
Vinicius Claudino <gnu.vinicius@gmail.com>
2026-03-22 07:25:37 -0300

    wip

--- ms-recommendation/core/settings/base.py
+++ ms-recommendation/core/settings/base.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Service is exposed behind API Gateway under /recommendation.
+STATIC_URL = '/recommendation/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
